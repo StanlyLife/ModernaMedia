770d8b7912e205d521f77a71fc183426d7c39d81
Stian have <stianhave@hotmail.com>
2021-04-03 19:20:45 +0200

    Created navbar

---
+++ ModernaMediaAngular/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Injector, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(private injector: Injector) {}
+
+  ngOnInit(): void {
+  }
+
+}
