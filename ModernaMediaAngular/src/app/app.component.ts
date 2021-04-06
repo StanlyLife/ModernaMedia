50d967eedb31d2ab80e1339b542a8e648827892d
Stian have <stianhave@hotmail.com>
2021-04-06 17:29:09 +0200

    Added fas icons

--- ModernaMediaAngular/src/app/app.component.ts
+++ ModernaMediaAngular/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { AppService } from './app.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Meta, Title } from '@angular/platform-browser';
-import { faAppleAlt, faCog, faGlobe, faMobile, faProcedures, faStore } from '@fortawesome/free-solid-svg-icons';
+import { faAppleAlt, faAtlas, faBook, faCog, faCogs, faCrop, faCropAlt, faDesktop, faFileArchive, faFileCode, faGlobe, faGlobeAmericas, faImage, faMobile, faOtter, faPaintBrush, faProcedures, faStore, faTrademark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,15 @@ export class AppComponent implements OnInit {
   faglobe = faGlobe;
   faapp = faMobile;
   fasoftware = faCog;
+
+  falogo = faTrademark;
+  fawebdesign = faCropAlt;
+  fannet = faGlobeAmericas;
   
+  facontent = faBook;
+  faonpage = faFileCode;
+  faoffpage = faFileArchive;
+
   constructor(
     private as: AppService,
     private meta: Meta,
