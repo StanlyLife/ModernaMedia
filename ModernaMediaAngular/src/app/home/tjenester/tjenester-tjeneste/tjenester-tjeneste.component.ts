dbb735437530b1dfa7a1706007322f4aebb97901
Stian have <stianhave@hotmail.com>
2021-09-28 21:53:40 +0200

    extracted card component from tjenester component

---
+++ ModernaMediaAngular/src/app/home/tjenester/tjenester-tjeneste/tjenester-tjeneste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tjenester-tjeneste',
+  templateUrl: './tjenester-tjeneste.component.html',
+  styleUrls: ['./tjenester-tjeneste.component.scss']
+})
+export class TjenesterTjenesteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
