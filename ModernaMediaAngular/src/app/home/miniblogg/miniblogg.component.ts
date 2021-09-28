184675d3a44df5998697312688c0274ced95384d
Stian have <stianhave@hotmail.com>
2021-09-28 20:23:53 +0200

    added miniblogg desktop

--- ModernaMediaAngular/src/app/home/miniblogg/miniblogg.component.ts
+++ ModernaMediaAngular/src/app/home/miniblogg/miniblogg.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-miniblogg',
   templateUrl: './miniblogg.component.html',
-  styleUrls: ['./miniblogg.component.scss']
+  styleUrls: ['./miniblogg.component.scss','./miniblogg.desktop.component.scss']
 })
 export class MinibloggComponent implements OnInit {
 
