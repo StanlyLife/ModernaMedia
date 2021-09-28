cffc836c36fdb4c35b41095b974eccd171d069c7
Stian have <stianhave@hotmail.com>
2021-09-28 20:24:00 +0200

    added tjenester desktop

--- ModernaMediaAngular/src/app/home/tjenester/tjenester.component.ts
+++ ModernaMediaAngular/src/app/home/tjenester/tjenester.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tjenester',
   templateUrl: './tjenester.component.html',
-  styleUrls: ['./tjenester.component.scss']
+  styleUrls: ['./tjenester.component.scss','./tjenester.desktop.component.scss']
 })
 export class TjenesterComponent implements OnInit {
 
