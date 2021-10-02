5333f2278539edc7f6dbe04a798213361fc03506
Stian have <stianhave@hotmail.com>
2021-10-02 18:19:07 +0200

    initial commit for miniblogg-blogs

---
+++ ModernaMediaAngular/src/app/home/miniblogg/miniblogg-blogs/miniblogg-blogs.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-miniblogg-blogs',
+  templateUrl: './miniblogg-blogs.component.html',
+  styleUrls: ['./miniblogg-blogs.component.scss']
+})
+export class MinibloggBlogsComponent implements OnInit {
+
+  @Input() src;
+  @Input() title;
+  @Input() content;
+  @Input() href;
+  @Input() alt;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
