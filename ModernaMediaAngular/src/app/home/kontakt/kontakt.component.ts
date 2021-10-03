4a80ff6224afc7dfafe9117b7f7f298a19668ad1
Stian have <stianhave@hotmail.com>
2021-10-03 15:40:43 +0200

    added kontakt.desktop

--- ModernaMediaAngular/src/app/home/kontakt/kontakt.component.ts
+++ ModernaMediaAngular/src/app/home/kontakt/kontakt.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-kontakt',
   templateUrl: './kontakt.component.html',
-  styleUrls: ['./kontakt.component.scss']
+  styleUrls: ['./kontakt.component.scss', './kontakt.desktop.component.scss']
 })
 export class KontaktComponent implements OnInit {
 
