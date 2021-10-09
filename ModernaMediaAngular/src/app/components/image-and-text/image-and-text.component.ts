38e98983a741dd16c1e6d197d1e9045b0b501729
Stian have <stianhave@hotmail.com>
2021-10-09 21:32:29 +0200

    created new component for image-and-text

---
+++ ModernaMediaAngular/src/app/components/image-and-text/image-and-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-and-text',
+  templateUrl: './image-and-text.component.html',
+  styleUrls: ['./image-and-text.component.scss']
+})
+export class ImageAndTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
