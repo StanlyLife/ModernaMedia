16278a0b644cd59ebd94e7e9ca098530d97d7490
Stian have <stianhave@hotmail.com>
2021-10-09 21:32:40 +0200

    created new component for info-and-image

---
+++ ModernaMediaAngular/src/app/components/info-and-image/info-and-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-and-image',
+  templateUrl: './info-and-image.component.html',
+  styleUrls: ['./info-and-image.component.scss']
+})
+export class InfoAndImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
