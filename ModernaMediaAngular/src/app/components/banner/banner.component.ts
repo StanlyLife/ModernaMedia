5a7faff6693eddc1f12587a0f74a57dc42a15f75
Stian have <stianhave@hotmail.com>
2021-10-09 23:48:07 +0200

    created banner component

--- ModernaMediaAngular/src/app/components/banner/banner.component.ts
+++ ModernaMediaAngular/src/app/components/banner/banner.component.ts
@@ -1,11 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss', './banner.desktop.component.scss']
 })
 export class BannerComponent implements OnInit {
+  public UniqueSellingPoints = [
+    {
+      'icon' : "../../../assets/Images/Icons/rocket-9959.svg",
+      'title' : "1Rakkett løsning",
+      "content" : "Raske løsninger for raske briller"
+    },
+    {
+      'icon' : "../../../assets/Images/Icons/rocket-9959.svg",
+      'title' : "2Rakkett løsning",
+      "content" : "Raske løsninger for raske briller"
+    },
+    {
+      'icon' : "../../../assets/Images/Icons/rocket-9959.svg",
+      'title' : "3Rakkett løsning",
+      "content" : "Raske løsninger for raske briller"
+    },
+    {
+      'icon' : "../../../assets/Images/Icons/rocket-9959.svg",
+      'title' : "4Rakkett løsning",
+      "content" : "Raske løsninger for raske briller"
+    },
+    {
+      'icon' : "../../../assets/Images/Icons/rocket-9959.svg",
+      'title' : "5Rakkett løsning",
+      "content" : "Raske løsninger for raske briller"
+    },
+    {
+      'icon' : "../../../assets/Images/Icons/rocket-9959.svg",
+      'title' : "6Rakkett løsning",
+      "content" : "Raske løsninger for raske briller Raske løsninger for"
+    }
+    
+  ];
+
+  @Input() sectionClass:string;
+
 
   constructor() { }
 
