41117175aedff541314bf7106444c3c337967297
Stian have <stianhave@hotmail.com>
2021-10-10 18:24:31 +0200

    added sectionStyle

--- ModernaMediaAngular/src/app/components/image-and-text/image-and-text.component.ts
+++ ModernaMediaAngular/src/app/components/image-and-text/image-and-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-and-text',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ImageAndTextComponent implements OnInit {
 
   constructor() { }
-
+  @Input() sectionStyle:string = 'dark';
   ngOnInit(): void {
   }
 
