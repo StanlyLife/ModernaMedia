9dc06572af680ba0bf3df225b6cc1571ec700ae5
Stian have <stianhave@hotmail.com>
2021-10-10 18:23:58 +0200

    Initial commit for finished mobile design

--- ModernaMediaAngular/src/app/components/info-and-image/info-and-image.component.ts
+++ ModernaMediaAngular/src/app/components/info-and-image/info-and-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info-and-image',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class InfoAndImageComponent implements OnInit {
 
   constructor() { }
-
+  @Input() sectionStyle:string = 'light';
   ngOnInit(): void {
   }
 
