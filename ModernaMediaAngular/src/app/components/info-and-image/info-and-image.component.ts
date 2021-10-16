f095d2caa8e8d0226e2e8b42daefaf85a1bb245f
Stian have <stianhave@hotmail.com>
2021-10-16 16:12:09 +0200

    added desktop css file

--- ModernaMediaAngular/src/app/components/info-and-image/info-and-image.component.ts
+++ ModernaMediaAngular/src/app/components/info-and-image/info-and-image.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-info-and-image',
   templateUrl: './info-and-image.component.html',
-  styleUrls: ['./info-and-image.component.scss']
+  styleUrls: ['./info-and-image.component.scss', './info-and-image.desktop.component.scss']
 })
 export class InfoAndImageComponent implements OnInit {
 
