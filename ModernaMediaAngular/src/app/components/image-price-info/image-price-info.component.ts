960f013d75a61514f28fa1210785df2defc51acb
Stian have <stianhave@hotmail.com>
2021-10-16 18:50:18 +0200

    changed content text for tests and added desktop styles

--- ModernaMediaAngular/src/app/components/image-price-info/image-price-info.component.ts
+++ ModernaMediaAngular/src/app/components/image-price-info/image-price-info.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-image-price-info',
   templateUrl: './image-price-info.component.html',
-  styleUrls: ['./image-price-info.component.scss']
+  styleUrls: ['./image-price-info.component.scss','./image-price-info.desktop.component.scss']
 })
 export class ImagePriceInfoComponent implements OnInit {
   @Input() componentTheme = 'dark';
@@ -28,7 +28,7 @@ export class ImagePriceInfoComponent implements OnInit {
       'imageSrc': "../../../assets/Images/Components/usp-image-cards/Farge kombinasjoner for design.jpg",
       'imageAlt': "Alt tekst",
       'Subtitle': "Vi tar ikke betalt før produktet er ferdig, på denne måten er du helt <span>fornøyd</span>, helt <span>risikofritt</span>",
-      "Content": "transmitting quantify navigating the card won't do anything, we need to transmit the optical PCI sensor! calculate The HDD driver is down, back up the cross-platform interface so we can override the RSS system! Use the mobile XML microchip, then you can copy the back-end program! You can't connect the sensor without backing up the bluetooth HTTP hard drive! transmitting the system won't do anything, we need to input the 1080p THX program! Try to navigate the PNG port, maybe it will input the online program! I'll hack the 1080p CSS firewall, that should transmitter the PCI monitor! backing up You can't input the program without synthesizing the bluetooth FTP capacitor! ",
+      "Content": "Vi utvikler nettsider og systemer for bedrifter av alle størrelser. En mindre bedrift har like stort behov for en nettside som en stor bedrift, men har ikke behov for en nettside av samme størrelse som en større bedrift",
       "Icons": [
         {
           "IconSrc": "../../../assets/Images/Icons/rocket-9959.svg",
