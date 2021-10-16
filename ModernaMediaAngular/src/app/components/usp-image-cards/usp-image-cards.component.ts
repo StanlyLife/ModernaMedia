90704cc96bf696354146aec390159de5a1be6c8e
Stian have <stianhave@hotmail.com>
2021-10-16 16:50:36 +0200

    added desktop css

--- ModernaMediaAngular/src/app/components/usp-image-cards/usp-image-cards.component.ts
+++ ModernaMediaAngular/src/app/components/usp-image-cards/usp-image-cards.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-usp-image-cards',
   templateUrl: './usp-image-cards.component.html',
-  styleUrls: ['./usp-image-cards.component.scss']
+  styleUrls: ['./usp-image-cards.component.scss', './usp-image-cards.desktop.component.scss']
 })
 export class UspImageCardsComponent implements OnInit {
 
