fc354004a4ea0656aa0eddf4b3a230f804d6275d
Stian have <stianhave@hotmail.com>
2021-10-18 19:10:56 +0200

    updated canoical url

--- ModernaMediaAngular/src/app/utvikling/utvikling/utvikling.component.ts
+++ ModernaMediaAngular/src/app/utvikling/utvikling/utvikling.component.ts
@@ -1,3 +1,4 @@
+import { SeoService } from './../../services/seo.service';
 import { Meta, Title } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,12 +11,14 @@ export class UtviklingComponent implements OnInit {
 
   constructor(
     private meta: Meta,
-    private title: Title
+    private title: Title,
+    private seo: SeoService
     ) {
       title.setTitle("Moderna Media Utvikling av nettsider og system for bedrifter");
     }
 
    ngOnInit() {
+     this.seo.createLinkForCanonicalURL();
      this.meta.addTags([
        {name: "description", content: "Moderna Media utvikler nettsider og system for bedrifter som ønsker et eget nettsted, system eller app"},
        {name: 'robots', content: 'index, follow'}
