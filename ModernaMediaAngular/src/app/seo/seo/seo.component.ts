141e8fc0f4709fbbb68bfceb98b87c83ab15d662
Stian have <stianhave@hotmail.com>
2021-10-21 20:09:05 +0200

    initial commit for seo component

---
+++ ModernaMediaAngular/src/app/seo/seo/seo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seo',
+  templateUrl: './seo.component.html',
+  styleUrls: ['./seo.component.scss']
+})
+export class SeoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
