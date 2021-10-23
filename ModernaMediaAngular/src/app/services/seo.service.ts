c3ad2a7fdf41f2861fba008c038bf23978cf7af6
Stian have <stianhave@hotmail.com>
2021-10-23 21:21:04 +0200

    added https check to canoical

--- ModernaMediaAngular/src/app/services/seo.service.ts
+++ ModernaMediaAngular/src/app/services/seo.service.ts
@@ -8,8 +8,11 @@ export class SeoService {
   
   createLinkForCanonicalURL() {
      let link: HTMLLinkElement = this.doc.createElement('link');
-     link.setAttribute('rel', 'canonical');
-     this.doc.head.appendChild(link);
-     link.setAttribute('href', this.doc.URL);
+     if((link + "").startsWith("https://") ){
+
+       link.setAttribute('rel', 'canonical');
+       this.doc.head.appendChild(link);
+       link.setAttribute('href', this.doc.URL);
+      }
    }
 }
