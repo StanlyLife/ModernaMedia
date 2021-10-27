55a254cc5fcc51aedb4e8055d76663d6ffc7c6fa
Stian have <stianhave@hotmail.com>
2021-10-27 21:48:42 +0200

    Created blogg thumbnail design for components

--- ModernaMediaAngular/src/app/components/blogg-thumbnail/blogg-thumbnail.component.ts
+++ ModernaMediaAngular/src/app/components/blogg-thumbnail/blogg-thumbnail.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from './../../../environments/environment.prod';
 
 @Component({
   selector: 'app-blogg-thumbnail',
   templateUrl: './blogg-thumbnail.component.html',
   styleUrls: ['./blogg-thumbnail.component.scss']
 })
+
+
+
+
 export class BloggThumbnailComponent implements OnInit {
+
+  @Input() src;
+  @Input() title;
+  @Input() content;
+  @Input() href = '';
+  @Input() alt;
+  @Input() theme;
+
+  imageCdn = environment.img;
 
   constructor() { }
 
