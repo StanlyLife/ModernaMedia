9ade7ef5a07372835b315d13cea58900a5ea61a4
Stian have <stianhave@hotmail.com>
2021-10-30 18:01:52 +0200

    bug fixes

--- ModernaMediaAngular/src/app/about/about/about.component.ts
+++ ModernaMediaAngular/src/app/about/about/about.component.ts
@@ -18,8 +18,10 @@ export class AboutComponent implements OnInit {
       title.setTitle("Vår historie - Moderna media digitalbyrå med fokus på webutvikling, design og seo for bedrifter");
     }
     imageCdn = environment.img;
+    public vw: any = 500;
    ngOnInit(): void {
-     this.seo.createLinkForCanonicalURL();
+    this.vw = window.innerWidth < window.innerHeight ? window.innerHeight : window.innerWidth;
+    this.seo.createLinkForCanonicalURL();
      this.meta.addTags([
        {name: "description", content: "Moderna Media utvikler nettsider og system for bedrifter som ønsker et eget nettsted, et tilpasset system eller en egen app. Vi har fokus og ekspertise innenfor 'system utvikling', 'web utvikling', 'søkemotoroptomalisering', 'grafisk design', 'webdesign' og mye mer. Les mer om hvorfor vi er det beste digitalbyrået i Norge"},
        {name: 'robots', content: 'index, follow'}
