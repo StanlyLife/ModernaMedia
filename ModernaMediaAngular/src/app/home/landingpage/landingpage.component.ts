ea4db9ef3edc507ab3e8ed6584196ecf262a42af
Stian have <stianhave@hotmail.com>
2021-11-23 21:56:19 +0100

    added CTA action to service

--- ModernaMediaAngular/src/app/home/landingpage/landingpage.component.ts
+++ ModernaMediaAngular/src/app/home/landingpage/landingpage.component.ts
@@ -1,18 +1,29 @@
+import { CTAMeetingService } from './../../services/ctameeting.service';
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../../../environments/environment.prod';
 
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
-  styleUrls: ['./landingpage.component.scss','./landingpage.desktop.component.scss']
+  styleUrls: [
+    './landingpage.component.scss',
+    './landingpage.desktop.component.scss',
+  ],
 })
 export class LandingpageComponent implements OnInit {
   imageCdn = environment.img;
-  constructor() { }
+  constructor(private ctaService: CTAMeetingService) {}
 
   public vw: any = 500;
-  ngOnInit() {
-      this.vw = window.innerWidth < window.innerHeight ? window.innerHeight : window.innerWidth;
+
+  ToggleCTA() {
+    this.ctaService.ToggleModal();
   }
 
+  ngOnInit() {
+    this.vw =
+      window.innerWidth < window.innerHeight
+        ? window.innerHeight
+        : window.innerWidth;
+  }
 }
