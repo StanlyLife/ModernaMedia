2dc0fa445c3809248cb849ff67414886981bd935
Stian have <stianhave@hotmail.com>
2021-11-23 21:56:34 +0100

    auto format

--- ModernaMediaAngular/src/app/home/home.component.ts
+++ ModernaMediaAngular/src/app/home/home.component.ts
@@ -5,28 +5,32 @@ import { Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-
   constructor(
     private meta: Meta,
     private title: Title,
     private seo: SeoService
-    ) {
-      title.setTitle("Moderna Media Digitalbyrå | Bedrift nettsider, systemer, webdesign og seo");
-    }
+  ) {
+    title.setTitle(
+      'Moderna Media Digitalbyrå | Bedrift nettsider, systemer, webdesign og seo'
+    );
+  }
 
-   ngOnInit() {
+  ngOnInit() {
     this.seo.createLinkForCanonicalURL();
-     this.meta.addTags([
-       {name: "description", content: "Moderna Media utvikler nettsider og system for bedrifter som ønsker et eget nettsted, system eller app. Vi er også eksperter på SEO, Google analytics, google search og ikke minst design og web design. Et digitalbyrå med alt på et sted!"},
-       {name: 'robots', content: 'index, follow'}
-     ]);
-     this.meta.addTag({
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Moderna Media utvikler nettsider og system for bedrifter som ønsker et eget nettsted, system eller app. Vi er også eksperter på SEO, Google analytics, google search og ikke minst design og web design. Et digitalbyrå med alt på et sted!',
+      },
+      { name: 'robots', content: 'index, follow' },
+    ]);
+    this.meta.addTag({
       name: 'angular.ModernaMedia',
-      content: 'ModernaMedia'
+      content: 'ModernaMedia',
     });
     // this.meta.updateTag(
     //   {
@@ -34,9 +38,6 @@ export class HomeComponent implements OnInit {
     //     content: 'Cette application a été développée avec Angular version 11.0.0 et bootstrap 4.5.3' +
     //       ' Elle applique le Routing, le Lazy loading, le Server side rendering et les Progressive Web App (PWA)'
     //   });
-  }  
-  scrollToElement($element): void {
-
   }
-
+  scrollToElement($element): void {}
 }
