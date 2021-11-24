0cc46203d87d02ed3450930262e2619527ee3ea8
Stian have <stianhave@hotmail.com>
2021-11-24 21:41:28 +0100

    Added CTA controller with book a meeting endpoint

---
+++ ModernaMediaDotNet/ModernaMediaDotNet/ModernaMediaDotNet/Controllers/CTAController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModernaMediaDotNet.Services.Contract;
+using System.Threading.Tasks;
+using ModernaMediaDotNet.Models.CTA;
+
+namespace ModernaMediaDotNet.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CTAController : ControllerBase
+    {
+        private ITwillioService twillioService;
+
+        public CTAController(ITwillioService twillioService)
+        {
+            this.twillioService = twillioService;
+        }
+
+        [HttpPost]
+        public bool BookAMeeting(CTA model)
+        {
+            string body = $"Melding fra: {model.Email} - {model.Phonenumber} : \n {model.Body}";
+            var result = twillioService.SendMessage(body);
+            return result;
+        }
+    }
+}
