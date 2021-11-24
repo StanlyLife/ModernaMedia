d4bacb2adbe0c7b209fed7a613ddc833fe000b83
Stian have <stianhave@hotmail.com>
2021-11-24 21:43:06 +0100

    Created CTA model

---
+++ ModernaMediaDotNet/ModernaMediaDotNet/ModernaMediaDotNet/Models/CTA/CTA.cs
@@ -0,0 +1,10 @@
+﻿namespace ModernaMediaDotNet.Models.CTA
+{
+    public class CTA
+    {
+        public string Email { get; set; }
+        public string Phonenumber{ get; set; }
+        public string Body{ get; set; }
+
+    }
+}
