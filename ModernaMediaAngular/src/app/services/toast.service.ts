7210d546ad7074caf51a5a3730b62865f3134187
Stian have <stianhave@hotmail.com>
2021-11-27 20:39:51 +0100

    Updated toast service

--- ModernaMediaAngular/src/app/services/toast.service.ts
+++ ModernaMediaAngular/src/app/services/toast.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { toast } from '../components/toast/toast.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToastService {
+  public Toasts = new BehaviorSubject<Array<object>>([]);
 
-  constructor() { }
+  constructor() {}
+
+  Toast(Title: string, Message?: string, Style?: string, Timer?: number) {
+    const toastModel = new toast({
+      Title: Title,
+      Content: Message,
+      Timer: Timer,
+      Style: Style,
+      TimeLeft: Timer,
+      Count: 1,
+      PercentageCompleted: 100,
+    });
+    this.AddToast(toastModel);
+  }
+
+  private AddToast(toast: toast) {
+    const currentArr = this.Toasts.value;
+    const updatedToast = [...currentArr, toast];
+    let timer = setInterval(function () {
+      toast.PercentageCompleted = toast.TimeLeft / (toast.Timer / 100);
+      toast.TimeLeft = toast.TimeLeft - 10;
+      if (toast.TimeLeft <= 0 || !toast.TimeLeft) {
+        clearInterval(timer);
+      }
+    }, 10);
+    this.Toasts.next(updatedToast);
+  }
 }
