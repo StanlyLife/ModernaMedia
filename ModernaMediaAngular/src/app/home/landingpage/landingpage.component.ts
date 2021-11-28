372806b140af2970f9352164e281e200b75e62ea
Stian have <stianhave@hotmail.com>
2021-11-28 16:14:17 +0100

    removed toast service

--- ModernaMediaAngular/src/app/home/landingpage/landingpage.component.ts
+++ ModernaMediaAngular/src/app/home/landingpage/landingpage.component.ts
@@ -13,15 +13,11 @@ import { environment } from './../../../environments/environment.prod';
 })
 export class LandingpageComponent implements OnInit {
   imageCdn = environment.img;
-  constructor(
-    private ctaService: CTAMeetingService,
-    private toast: ToastService
-  ) {}
+  constructor(private ctaService: CTAMeetingService) {}
 
   public vw: any = 500;
 
   ToggleCTA() {
-    this.toast.Toast('Hello', 'This is content', 'Default', 2500);
     this.ctaService.ToggleModal();
   }
 
