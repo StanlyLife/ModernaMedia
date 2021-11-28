b01b24a9d788f6dc8a0c0174fa4a3e3e6074acf0
Stian have <stianhave@hotmail.com>
2021-11-28 16:16:51 +0100

    Added toasts to CTA service and fixed local storage issue

--- ModernaMediaAngular/src/app/services/ctameeting.service.ts
+++ ModernaMediaAngular/src/app/services/ctameeting.service.ts
@@ -1,3 +1,4 @@
+import { ToastService } from './toast.service';
 import { environment } from './../../environments/environment';
 import { CTA } from './../Models/CTA/CTA.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -11,7 +12,7 @@ export class CTAMeetingService {
   public ModalOpen = new BehaviorSubject<boolean>(false);
   public errorMessage;
   private contactRequests: number = 0;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private toast: ToastService) {}
   headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   private SendCTAMessageURL = environment.url + '/API/CTA/BookAMeeting';
   ToggleModal() {
@@ -21,18 +22,42 @@ export class CTAMeetingService {
   SendCTAMessage(body: CTA) {
     this.contactRequests = parseInt(localStorage.getItem('ContactRequests'));
     if (this.contactRequests >= 3) {
+      this.toast.Toast(
+        'For mange forespørsler',
+        'Du har send for mange forespørsler!',
+        'error',
+        5000
+      );
       return false;
     }
-    localStorage.setItem('ContactRequests', this.contactRequests + 1 + '');
     var request = this.http.post<CTA>(this.SendCTAMessageURL, body);
     console.log(this.SendCTAMessageURL);
     request.subscribe({
       next: (data) => {
-        console.log(this);
+        if (isNaN(this.contactRequests)) {
+          localStorage.setItem('ContactRequests', '1');
+        } else {
+          localStorage.setItem(
+            'ContactRequests',
+            this.contactRequests + 1 + ''
+          );
+        }
+        this.toast.Toast(
+          'Melding sendt!',
+          'Vi kontakter deg snarest!',
+          'default',
+          5000
+        );
       },
       error: (error) => {
         this.errorMessage = error.message;
         console.error('There was an error!', error);
+        this.toast.Toast(
+          'Det oppstod en feil!',
+          'Kontakt oss på tlf: 902 65 326!',
+          'error',
+          10000
+        );
       },
     });
     return request;
