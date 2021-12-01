7264404ecd3be51452bc97cc12a9d0d6607d1c61
Stian have <stianhave@hotmail.com>
2021-12-01 21:01:23 +0100

    initial commit for developer salary charts

---
+++ ModernaMediaAngular/src/app/tools/developer-salaray-charts/developer-salaray-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-developer-salaray-charts',
+  templateUrl: './developer-salaray-charts.component.html',
+  styleUrls: ['./developer-salaray-charts.component.scss']
+})
+export class DeveloperSalarayChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
