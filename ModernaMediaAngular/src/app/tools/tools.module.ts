9ce26fbae015d2be89c3dd203a77f7288aafd9d9
Stian have <stianhave@hotmail.com>
2021-12-02 21:35:09 +0100

    added ngx charts

--- ModernaMediaAngular/src/app/tools/tools.module.ts
+++ ModernaMediaAngular/src/app/tools/tools.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DeveloperSalarayChartsComponent } from './developer-salaray-charts/developer-salaray-charts.component';
 import { ComponentsModule } from '../components/components.module';
 import { RouterModule } from '@angular/router';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [DeveloperSalarayChartsComponent],
   imports: [
     CommonModule,
     ComponentsModule,
+    NgxChartsModule,
     RouterModule.forRoot([
       {
         path: 'v/utviklerlonn',
