458b773165f5289efac6d5bfb4dca3d4ad331cd2
Stian have <stianhave@hotmail.com>
2021-12-16 22:10:35 +0100

    added robots.txt to SSR

--- ModernaMediaAngular/server.ts
+++ ModernaMediaAngular/server.ts
@@ -6,19 +6,22 @@ import { join } from 'path';
 import 'zone.js/dist/zone-node';
 import { AppServerModule } from './src/main.server';
 
-
-
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
   //server.use(compression());
   const distFolder = join(process.cwd(), 'dist/ModernaMediaAngular/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
-  const robotsTxt = 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? 'index.original.html'
+    : 'index';
+  const robotsTxt =
+    // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+    server.engine(
+      'html',
+      ngExpressEngine({
+        bootstrap: AppServerModule,
+      })
+    );
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
@@ -26,18 +29,27 @@ export function app() {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
   });
-  
-  // server.get('/robots.txt', (req, res) => {
-  //   res.render("test", { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
-  // });
+
+  server.get('/robots.txt', (req, res) => {
+    res.render('test', {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
+  });
 
   return server;
 }
@@ -57,10 +69,9 @@ function run() {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
 
 export * from './src/main.server';
-
