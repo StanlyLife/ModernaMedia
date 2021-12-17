b3b0a2c85e811d44c9bd7c3027afbc1df8e23100
Stian have <stianhave@hotmail.com>
2021-12-17 21:00:18 +0100

    added compression

--- ModernaMediaAngular/server.ts
+++ ModernaMediaAngular/server.ts
@@ -5,11 +5,12 @@ import { existsSync } from 'fs';
 import { join } from 'path';
 import 'zone.js/dist/zone-node';
 import { AppServerModule } from './src/main.server';
+import * as compression from 'compression';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  //server.use(compression());
+  server.use(compression());
   const distFolder = join(process.cwd(), 'dist/ModernaMediaAngular/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? 'index.original.html'
