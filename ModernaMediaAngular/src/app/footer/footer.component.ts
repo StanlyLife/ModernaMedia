907f262047b0794aeaf584c8fcd66c0c7e167f22
Stian have <stianhave@hotmail.com>
2021-12-19 22:52:23 +0100

    Added hidden property for certain pages

--- ModernaMediaAngular/src/app/footer/footer.component.ts
+++ ModernaMediaAngular/src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { WindowRefService } from './../services/window-ref.service';
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../../environments/environment.prod';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
@@ -8,9 +9,14 @@ import { environment } from './../../environments/environment.prod';
   styleUrls: ['./footer.component.scss', './footer.desktop.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor(private wf: WindowRefService) {}
+  constructor(private wf: WindowRefService, private location: Location) {}
   imageCdn = environment.img;
-  ngOnInit(): void {}
+  hidden = false;
+  ngOnInit(): void {
+    if (this.location.path() === '/blogg/utviklerl%C3%B8nn') {
+      this.hidden = true;
+    }
+  }
 
   scrollToElement($element): void {
     $element.scrollIntoView({
