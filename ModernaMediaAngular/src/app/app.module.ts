794ac80fa284f5395e9be5f359048d44573de38e
Stian have <stianhave@hotmail.com>
2022-02-14 19:52:33 +0100

    added components for home v2

--- ModernaMediaAngular/src/app/app.module.ts
+++ ModernaMediaAngular/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
 import { DesignModule } from './design/design.module';
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/homev2/home/home.component';
 import { KontaktComponent } from './home/kontakt/kontakt.component';
 import { LandingpageComponent } from './home/landingpage/landingpage.component';
 import { MinibloggBlogsComponent } from './home/miniblogg/miniblogg-blogs/miniblogg-blogs.component';
@@ -27,6 +27,12 @@ import { TjenesterComponent } from './home/tjenester/tjenester.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SeoModule } from './seo/seo.module';
 import { UtviklingModule } from './utvikling/utvikling.module';
+import { LandingComponent } from './home/homev2/landing/landing.component';
+import { ServicesComponent } from './home/homev2/services/services.component';
+import { AboutComponent } from './home/homev2/about/about.component';
+import { ContactComponent } from './home/homev2/contact/contact.component';
+import { UspComponent } from './home/homev2/usp/usp.component';
+import { PricesComponent } from './home/homev2/prices/prices.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +47,12 @@ import { UtviklingModule } from './utvikling/utvikling.module';
     TjenesterTjenesteComponent,
     MinibloggBlogsComponent,
     KontaktComponent,
+    LandingComponent,
+    ServicesComponent,
+    AboutComponent,
+    ContactComponent,
+    UspComponent,
+    PricesComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
