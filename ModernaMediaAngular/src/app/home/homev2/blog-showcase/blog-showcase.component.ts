2a05f84a9922ec7570fa7eba02eeaf055d91252e
Stian have <stianhave@hotmail.com>
2022-02-15 20:29:41 +0100

    Added blog showcase for home v2

---
+++ ModernaMediaAngular/src/app/home/homev2/blog-showcase/blog-showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-showcase',
+  templateUrl: './blog-showcase.component.html',
+  styleUrls: [
+    './blog-showcase.component.scss',
+    './blog-showcase.desktop.component.scss',
+  ],
+})
+export class BlogShowcaseComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
