6fa69e6d87057b1a8f0f4beaa5da599407ca9101
Stian have <stianhave@hotmail.com>
2022-02-16 20:45:59 +0100

    added prices component

---
+++ ModernaMediaAngular/src/app/home/homev2/prices/prices.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prices',
+  templateUrl: './prices.component.html',
+  styleUrls: ['./prices.component.scss', './prices.desktop.component.scss'],
+})
+export class PricesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
