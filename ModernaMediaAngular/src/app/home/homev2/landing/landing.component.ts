5b514d6d40cd04a96112a3ea393e160a6825096a
Stian have <stianhave@hotmail.com>
2022-02-16 20:45:48 +0100

    added new landing page for home v2

---
+++ ModernaMediaAngular/src/app/home/homev2/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss', './landing.desktop.component.scss'],
+})
+export class LandingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
