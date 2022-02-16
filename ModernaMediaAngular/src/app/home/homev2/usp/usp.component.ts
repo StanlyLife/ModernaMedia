bd9fcac53b9299ba0d8a3382f284d792237fd277
Stian have <stianhave@hotmail.com>
2022-02-16 20:47:41 +0100

    added usp component for home v2

---
+++ ModernaMediaAngular/src/app/home/homev2/usp/usp.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usp',
+  templateUrl: './usp.component.html',
+  styleUrls: ['./usp.component.scss', './usp.desktop.component.scss'],
+})
+export class UspComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
