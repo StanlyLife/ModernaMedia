588a19e5706b057169c6a3d7420aee68108697da
Stian have <stianhave@hotmail.com>
2022-02-16 23:08:11 +0100

    added seo fix

--- ModernaMediaAngular/src/app/home/homev2/home/home.component.ts
+++ ModernaMediaAngular/src/app/home/homev2/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from './../../../services/seo.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private meta: Meta,
+    private title: Title,
+    private seo: SeoService
+  ) {
+    title.setTitle(
+      'Moderna Media Digitalbyrå | Bedrift nettsider, systemer, webdesign og seo'
+    );
   }
 
+  ngOnInit() {
+    this.seo.createLinkForCanonicalURL();
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Moderna Media utvikler nettsider og system for bedrifter som ønsker et eget nettsted, system eller app. Vi er også eksperter på SEO, Google analytics, google search og ikke minst design og web design. Et digitalbyrå med alt på et sted!',
+      },
+      { name: 'robots', content: 'index, follow' },
+    ]);
+    this.meta.addTag({
+      name: 'angular.ModernaMedia',
+      content: 'ModernaMedia',
+    });
+  }
 }
