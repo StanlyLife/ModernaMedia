d6d1ca734e5ba9331b07f123d43e010710c89264
Stian have <stianhave@hotmail.com>
2022-02-20 22:41:06 +0100

    added contact api

---
+++ ModernaMediaDotNet/ModernaMedia/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModernaMediaDotNet.Models.contact;
+using ModernaMediaDotNet.Services.Contract;
+
+namespace ModernaMediaDotNet.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+
+        private ITwillioService twillioService;
+
+        public ContactController(ITwillioService twillioService)
+        {
+            this.twillioService = twillioService;
+        }
+
+        [HttpPost]
+        public bool Contact(ContactModel model)
+        {
+            string body = $"Melding fra MODERNA MEDIA: \n" +
+                $"navn: {model.name} \n" +
+                $"epost: {model.email} \n" +
+                $"telefon: {model.phone} \n" +
+                $"bedrift: {model.business} \n" +
+                $"tittel: {model.title} \n" +
+                $"innhold: {model.body}";
+            var result = twillioService.SendMessage(body);
+            return result;
+        }
+    }
+}
---
+++ ModernaMediaDotNet/ModernaMedia/Models/Contact/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ModernaMediaDotNet.Models.contact
+{
+    public class ContactModel
+    {
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public string business { get; set; }
+        public string body { get; set; }
+    }
+}
