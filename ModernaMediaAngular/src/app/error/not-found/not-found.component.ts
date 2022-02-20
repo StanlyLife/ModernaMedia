fffc90abb937469cdc14f447259e4f34017408d0
Stian have <stianhave@hotmail.com>
2022-02-20 18:42:37 +0100

    created 404 page

--- ModernaMediaAngular/src/app/error/not-found/not-found.component.ts
+++ ModernaMediaAngular/src/app/error/not-found/not-found.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sanitizer: DomSanitizer) {}
+  sanitizeImageUrl(imageUrl: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
   }
-
+  imageCdn = environment.img;
+  ngOnInit(): void {}
 }
