e7948e998aadc9c8918a715e162cccd67c7a84e8
Stian have <stianhave@hotmail.com>
2022-02-21 17:25:08 +0100

    fixed import

--- ModernaMediaAngular/src/app/services/contact.service.ts
+++ ModernaMediaAngular/src/app/services/contact.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ToastService } from './toast.service';
-import { environment } from './../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
