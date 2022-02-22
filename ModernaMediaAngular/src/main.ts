a33854acd549232aa5ebbd52828c5ab848e673c4
Stian have <stianhave@hotmail.com>
2022-02-22 18:34:20 +0100

    removed console.log

--- ModernaMediaAngular/src/main.ts
+++ ModernaMediaAngular/src/main.ts
@@ -3,13 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-
 if (environment.production) {
   enableProdMode();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-  console.log("DOMCONTENTLOADED");
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
 });
