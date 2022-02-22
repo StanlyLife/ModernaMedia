6f3979fe55cea821b15cf8bdb6b0cb4e3d5189de
Stian have <stianhave@hotmail.com>
2022-02-22 19:02:28 +0100

    added misc and designsystem

---
+++ ModernaMediaAngular/src/app/misc/designsystem/designsystem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-designsystem',
+  templateUrl: './designsystem.component.html',
+  styleUrls: ['./designsystem.component.scss']
+})
+export class DesignsystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ModernaMediaAngular/src/app/misc/misc.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DesignsystemComponent } from './designsystem/designsystem.component';
+
+
+
+@NgModule({
+  declarations: [
+    DesignsystemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MiscModule { }
