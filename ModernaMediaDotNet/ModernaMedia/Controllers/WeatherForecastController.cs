c2e7ca612da4225ec02e33ca376b454f49b977a7
Stian have <stianhave@hotmail.com>
2022-02-22 17:57:50 +0100

    removed authorize attribute from test endpoint

--- ModernaMediaDotNet/ModernaMedia/Controllers/WeatherForecastController.cs
+++ ModernaMediaDotNet/ModernaMedia/Controllers/WeatherForecastController.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 
 namespace ModernaMediaDotNet.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
