64e8e739bd7276562bc59c0e3c8dd4d6baa6827a
Stian have <stianhave@hotmail.com>
2022-02-27 22:16:13 +0100

    added info section component

---
+++ ModernaMediaAngular/src/app/Tjenester/components/tjenester-info-section/tjenester-info-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tjenester-info-section',
+  templateUrl: './tjenester-info-section.component.html',
+  styleUrls: ['./tjenester-info-section.component.scss']
+})
+export class TjenesterInfoSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
