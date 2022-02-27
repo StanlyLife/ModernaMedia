d165a651f5441f4799e2ad80fbff5d9d925c6f74
Stian have <stianhave@hotmail.com>
2022-02-27 22:16:01 +0100

    added header component

---
+++ ModernaMediaAngular/src/app/Tjenester/components/tjenester-header/tjenester-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tjenester-header',
+  templateUrl: './tjenester-header.component.html',
+  styleUrls: ['./tjenester-header.component.scss']
+})
+export class TjenesterHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
