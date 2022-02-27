45d9959e587a322a41b387ea7af02fae95c1db33
Stian have <stianhave@hotmail.com>
2022-02-27 22:16:46 +0100

    added servicees component

---
+++ ModernaMediaAngular/src/app/Tjenester/components/tjenester-services/tjenester-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tjenester-services',
+  templateUrl: './tjenester-services.component.html',
+  styleUrls: ['./tjenester-services.component.scss']
+})
+export class TjenesterServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
