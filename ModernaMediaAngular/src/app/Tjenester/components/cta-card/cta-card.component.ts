1e2ec9c200ccfb28ef3abf930ae3c77a2728cd33
Stian have <stianhave@hotmail.com>
2022-02-27 22:15:54 +0100

    added CTA-card component

---
+++ ModernaMediaAngular/src/app/Tjenester/components/cta-card/cta-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cta-card',
+  templateUrl: './cta-card.component.html',
+  styleUrls: ['./cta-card.component.scss']
+})
+export class CtaCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
