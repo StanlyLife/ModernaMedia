97fab94a0d204484be1693f7529c7d616254dbe3
Stian have <stianhave@hotmail.com>
2022-02-28 22:03:41 +0100

    added header

--- ModernaMediaAngular/src/app/Tjenester/components/tjenester-header/tjenester-header.component.ts
+++ ModernaMediaAngular/src/app/Tjenester/components/tjenester-header/tjenester-header.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-tjenester-header',
   templateUrl: './tjenester-header.component.html',
-  styleUrls: ['./tjenester-header.component.scss']
+  styleUrls: [
+    './tjenester-header.component.scss',
+    './tjenester-header.desktop.component.scss',
+  ],
 })
 export class TjenesterHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sanitizer: DomSanitizer) {}
+  imageCdn = environment.img;
+  ngOnInit(): void {}
+  @Input() data: any = {
+    background: {
+      alt: '',
+      src: '../../../../assets/Images/Tjenester/header/mac.jpg',
+    },
+    icon: {
+      alt: '',
+      src: '../../../../assets/Images/Icons/tjenester/developer.svg',
+    },
+    title: 'Utvikling',
+    subtitle: 'Vi lager nettsider og system som øker omsetningen din',
+    links: [
+      { title: 'Tjenester', url: '/#services' },
+      { title: 'Prosess', url: '/#process' },
+      { title: 'System', url: '/#system' },
+      { title: 'Hjemmesider', url: '/#website' },
+    ],
+  };
+  sanitizeImageUrl(imageUrl: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
   }
-
 }
