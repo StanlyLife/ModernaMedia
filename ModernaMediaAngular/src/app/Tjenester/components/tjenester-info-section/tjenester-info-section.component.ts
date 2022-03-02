537cb1233879a4c2e62c8976fd4c6fde59a539b7
Stian have <stianhave@hotmail.com>
2022-03-02 18:45:27 +0100

    added info section

--- ModernaMediaAngular/src/app/Tjenester/components/tjenester-info-section/tjenester-info-section.component.ts
+++ ModernaMediaAngular/src/app/Tjenester/components/tjenester-info-section/tjenester-info-section.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-tjenester-info-section',
   templateUrl: './tjenester-info-section.component.html',
-  styleUrls: ['./tjenester-info-section.component.scss']
+  styleUrls: [
+    './tjenester-info-section.component.scss',
+    './tjenester-info-section.desktop.component.scss',
+  ],
 })
 export class TjenesterInfoSectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sanitizer: DomSanitizer) {}
+  imageCdn = environment.img;
+  ngOnInit(): void {}
+  @Input() data = {
+    position: 'right',
+    header: {
+      subtitle: 'Øk bedriftens omsettning',
+      title: 'Med nettside for bedriften',
+    },
+    button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
+    text: [
+      '1IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
+      '2IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
+      '3IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
+    ],
+  };
+  sanitizeImageUrl(imageUrl: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
   }
-
 }
