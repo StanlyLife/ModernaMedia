e56e147af6a76023c923b1cc204630e04d96d162
Stian have <stianhave@hotmail.com>
2022-03-04 11:06:27 +0100

    updated images

--- ModernaMediaAngular/src/app/Tjenester/seo/seo.component.ts
+++ ModernaMediaAngular/src/app/Tjenester/seo/seo.component.ts
@@ -13,7 +13,7 @@ export class SEOComponent implements OnInit {
     private seo: SeoService
   ) {
     title.setTitle(
-      'Moderna Media - Digitalbyrå for bedriftens hjemmeside, design og seo'
+      'Søkemotor optimalisering for bedriften din av Moderna Media SEO byrå'
     );
   }
 
@@ -36,50 +36,51 @@ export class SEOComponent implements OnInit {
   dataHeader: any = {
     background: {
       alt: '',
-      src: '../../../../assets/Images/Tjenester/header/design (1).jpg',
+      src: '../../../../assets/Images/Tjenester/header/seo/seo4.jpg',
     },
     icon: {
       alt: '',
-      src: '../../../../assets/Images/Icons/tjenester/grafisk_design.svg',
+      src: '../../../../assets/Images/Icons/tjenester/søkemotoroptimalisering byrå.svg',
     },
-    title: 'Design',
-    subtitle: 'Vi skaper vakre design med kunden i fokus',
+    title: 'søkemotoroptimalisering',
+    subtitle:
+      'Vi øker synlighet, vekst og omsetning gjennom søkemotor optimalisering for bedriften din',
     links: [
-      { title: 'Tjenester', url: 'services' },
-      { title: 'Grafisk design', url: 'grafisk_design' },
-      { title: 'Web design', url: 'web_design' },
-      { title: 'Logo design', url: 'logo_design' },
+      { title: 'tjenester', url: 'services' },
+      { title: 'teknisk seo', url: 'tekniskseo' },
+      { title: 'off-page seo', url: 'offpageseo' },
+      { title: 'innholdsproduksjon', url: 'innholdsproduksjon' },
     ],
   };
   dataServices: any = {
-    title: 'Web Design | Logo Design | Grafisk Design',
-    subtitle: 'Våre designtjenester for bedrifter.',
+    title: 'Web utvikling | System utvikling',
+    subtitle: 'Våre utviklingstjenester for bedrifter.',
     headerText:
       'Våre systemer og hjemmesider er bygget helt fra bunn med banebrytende teknologi som SPA rammeverk og full cloud-service',
     services: [
       {
-        title: 'Logo design',
-        text: 'virtual innovate interfaces front-end iterate networksfront-end unleash communities',
+        title: 'Sytemer for bedrifter',
+        text: 'Automatiser daglige gjøremål eller gjør livet enklere for kundene med våre skreddersydde system.',
         icon: {
-          src: '../../../../assets/Images/Icons/tjenester/services/logo_design.svg',
+          src: '../../../../assets/Images/Icons/tjenester/services/teknisk seo.svg',
           alt: '',
         },
         link: '',
       },
       {
-        title: 'Web design',
-        text: 'virtual innovate interfaces front-end iterate networksfront-end unleash communities',
+        title: 'Hjemmeside for bedrift',
+        text: 'Invester i en nettside for bedriften av topp kvalitet',
         icon: {
-          src: '../../../../assets/Images/Icons/tjenester/services/hjemmeside-bedrift.svg',
+          src: '../../../../assets/Images/Icons/tjenester/services/seo innholdsproduksjon.svg',
           alt: '',
         },
         link: '',
       },
       {
-        title: 'Grafisk design',
-        text: 'virtual innovate interfaces front-end iterate networksfront-end unleash communities',
+        title: 'Sytemer for bedrifter',
+        text: 'Automatiser daglige gjøremål eller gjør livet enklere for kundene med våre skreddersydde system.',
         icon: {
-          src: '../../../../assets/Images/Icons/tjenester/services/grafisk_design.svg',
+          src: '../../../../assets/Images/Icons/tjenester/services/off page seo.svg',
           alt: '',
         },
         link: '',
@@ -87,15 +88,15 @@ export class SEOComponent implements OnInit {
     ],
   };
   dataInfoSection = {
-    color: 'gray',
+    color: 'grey',
     img: {
-      url: '../../../../assets/Images/Tjenester/info-section/design (2).jpg',
+      url: '../../../../assets/Images/Tjenester/info-section/utvikling/utvikling.jpg',
       alt: '',
     },
-    position: 'right',
+    position: 'left',
     header: {
-      subtitle: 'Uttrykk deres verider med',
-      title: 'Grafisk design',
+      subtitle: 'Spar kostnader med',
+      title: 'Teknisk SEO',
     },
     button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
     text: [
@@ -105,39 +106,31 @@ export class SEOComponent implements OnInit {
     ],
   };
   dataInfoSection2 = {
-    color: 'grey',
-    img: {
-      url: '../../../../assets/Images/Tjenester/info-section/web_design_oslo.jpg',
-      alt: '',
-    },
-    position: 'left',
-    header: {
-      subtitle: 'Voks organisk med god',
-      title: 'Web design',
-    },
-    button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
-    text: [
-      '1IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-      '2IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-      '3IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-    ],
-  };
-  dataInfoSection3 = {
     color: 'gray',
     img: {
-      url: '../../../../assets/Images/Tjenester/info-section/logo designer i oslo som holder en iphone.jpg',
+      url: '../../../../assets/Images/Tjenester/info-section/seo/backlink søkemotor optimalisering.jpg',
       alt: '',
     },
     position: 'right',
     header: {
       subtitle: 'Bygg en sterk merkevare med',
-      title: 'Logo for bedriften',
+      title: 'Off-page SEO',
     },
     button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
-    text: [
-      '1IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-      '2IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-      '3IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-    ],
+    text: ['', '', ''],
+  };
+  dataInfoSection3 = {
+    color: 'gray',
+    img: {
+      url: '../../../../assets/Images/Tjenester/info-section/seo/innholdsproduksjon for søkemotor optimalisering.jpg',
+      alt: '',
+    },
+    position: 'right',
+    header: {
+      subtitle: 'Øk rangeringen med innholdsproduksjon',
+      title: 'On-page SEO',
+    },
+    button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
+    text: ['', '', ''],
   };
 }
