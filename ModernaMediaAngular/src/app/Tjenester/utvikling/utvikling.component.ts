7b890ddd7c3988b6bf8d1ade6c46b86bbc746f75
Stian have <stianhave@hotmail.com>
2022-03-04 21:10:28 +0100

    Updated content

--- ModernaMediaAngular/src/app/Tjenester/utvikling/utvikling.component.ts
+++ ModernaMediaAngular/src/app/Tjenester/utvikling/utvikling.component.ts
@@ -46,20 +46,20 @@ export class UtviklingComponent implements OnInit {
     subtitle:
       'Skreddersydde hjemmesider, system og applikasjoner for bedrifter',
     links: [
-      { title: 'services', url: 'services' },
-      { title: 'webutvikling', url: 'web utvikling' },
-      { title: 'systemutvikling', url: 'system utvikling' },
+      { title: 'Tjenester', url: 'services' },
+      { title: 'Webutvikling', url: 'web utvikling' },
+      { title: 'Systemutvikling', url: 'system utvikling' },
     ],
   };
   dataServices: any = {
     title: 'Web utvikling | System utvikling',
     subtitle: 'Våre utviklingstjenester for bedrifter.',
     headerText:
-      'Våre systemer og hjemmesider er bygget helt fra bunn med banebrytende teknologi som SPA rammeverk og full cloud-service',
+      'Våre datasystemer og hjemmesider er bygget helt fra bunn med banebrytende teknologi som SPA rammeverk og full cloud-service',
     services: [
       {
         title: 'Hjemmeside for bedrift',
-        text: 'Invester i en nettside for bedriften av topp kvalitet',
+        text: 'Invester i en nettside for bedriften. Av topp kvalitet.',
         icon: {
           src: '../../../../assets/Images/Icons/tjenester/services/hjemmeside-bedrift.svg',
           alt: '',
@@ -89,7 +89,13 @@ export class UtviklingComponent implements OnInit {
       title: 'Hjemmeside for bedriften',
     },
     button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
-    text: ['', '', ''],
+    text: [
+      'En bedrifts online tilstedeværelse, uavhengig av industri vil være kritisk for ens suksess. En bedrifts hjemmeside vil alene ikke være med på å skape tillit og vekst, men en attraktiv og komplett nettside vil ta deg en lang vei.',
+      'Har du noen gang besøkt en hjemmeside og tenkt “dette her kan ikke stemme?“ eller “Er dette trygt?“. Kanskje har du bestemt deg for å besøke en ny restaurant i Oslo, men du klarer ikke å finne hjemmesiden til bedriften, eller kanskje du finner den, men den inneholder ikke en meny. Hos digitalbyrået Moderna Media følger vi deg opp hele veien og sørger for at du får absolutt mest mulig for pengene.',
+      'Vi mener at en nettside har flere fordeler, blant annet vil den være med på å styrke merkevaren din, øke “leads“, skape organisk trafikk ved hjelp av god søkemotoroptimalisering som igjen vil øke din organsike vekst. En nettside kan også bygges på en måte som også sparer tid og penger.',
+      'Dersom nettsiden har “ofte stilte spørsmål og svar” lett tilgjengelig vil kunden kanskje ikke ha behov for å ringe eller sende melding. Dersom åpningstidene eller menyen ligger lett tilgjengelig slipper du å få samme telefon flere ganger daglig.',
+      'Alt i alt så har en bedrift behov for en hjemmeside, men ikke hvilken som helst hjemmeside, men en sikker, enkel, kompetent og attraktiv hjemmeside. En hjemmeside er ikke en kostnad, men en investering!',
+    ],
   };
   dataInfoSection2 = {
     color: 'grey',
@@ -100,13 +106,13 @@ export class UtviklingComponent implements OnInit {
     position: 'left',
     header: {
       subtitle: 'Spar kostnader med',
-      title: 'System for bedriften',
+      title: 'Datasystem for bedriften',
     },
     button: { text: 'Les mer om hjemmesider', url: '#tjenester' },
     text: [
-      '1IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-      '2IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
-      '3IPhone A/B testing research & development buzz leverage disruptive virality startup. Termsheet graphical user interface iPad funding long tail customer startup buyer beta. Gamification infrastructure bandwidth creative customer holy grail strategy channels value proposition iPad graphical user interface disruptive business plan niche market. Investor success bootstrapping scrum project twitter bandwidth mass market.',
+      'Et system kan være mange ting, vi bruker begrepet datasystem om en rekke funksjoner som blir utført sammen i et digitalt kommuniserende økosystem. Når restauranten tar imot en bestilling eller reservasjon på nett og deres interne datamaskiner oppdateres tilstrekkelig så brukes det et datasystem for å synkronisere disse jobbene.',
+      'Vi tilbyr våre kunder skreddersydde datasystem som er ment for å spare bedriften tid og penger. Ved å automatisere daglige gjøremål kan vi effektivt kutte kostandene samtidig som vi reduserer risikoen for menneskelig feil.',
+      'Vårt mål er å gjøre hverdagen din og deres kunder enklere. Dersom du skulle ha behov for et bookingsystem, drift- og logistikksystem eller et kundebehandlingssystem. Ta en prat med oss så finner vi den beste løsningen for bedriften din og deres budsjett.',
     ],
   };
 }
