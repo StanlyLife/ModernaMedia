a50a0fad863e1f9f001ef89b79968608c046cccc
Stian have <stianhave@hotmail.com>
2022-03-05 17:17:01 +0100

    Updated paths and added new components

--- ModernaMediaAngular/src/app/Tjenester/tjenester.module.ts
+++ ModernaMediaAngular/src/app/Tjenester/tjenester.module.ts
@@ -8,14 +8,54 @@ import { TjenesterHeaderComponent } from './components/tjenester-header/tjeneste
 import { TjenesterServicesComponent } from './components/tjenester-services/tjenester-services.component';
 import { TjenesterInfoSectionComponent } from './components/tjenester-info-section/tjenester-info-section.component';
 import { CtaCardComponent } from './components/cta-card/cta-card.component';
+import { UtviklingHjemmesideComponent } from './utvikling/utvikling-hjemmeside/utvikling-hjemmeside.component';
+import { UtviklingSystemComponent } from './utvikling/utvikling-system/utvikling-system.component';
+import { SeoTekniskComponent } from './seo/seo-teknisk/seo-teknisk.component';
+import { SeoInnholdsproduksjonComponent } from './seo/seo-innholdsproduksjon/seo-innholdsproduksjon.component';
+import { SeoOffPageComponent } from './seo/seo-off-page/seo-off-page.component';
+import { DesignGrafiskComponent } from './design/design-grafisk/design-grafisk.component';
+import { DesignLogoComponent } from './design/design-logo/design-logo.component';
+import { DesignWebComponent } from './design/design-web/design-web.component';
 
 const routes: Routes = [
   { path: 'tjenester/bedrift/design', component: DesignComponent },
   {
+    path: 'tjenester/bedrift/design/grafisk-design',
+    component: DesignGrafiskComponent,
+  },
+  {
+    path: 'tjenester/bedrift/design/logo-design',
+    component: DesignLogoComponent,
+  },
+  {
+    path: 'tjenester/bedrift/design/web-design',
+    component: DesignWebComponent,
+  },
+  {
     path: 'tjenester/bedrift/seo/søkemotoroptimalisering',
     component: SeoComponent,
   },
+  {
+    path: 'tjenester/bedrift/seo/teknisk-seo',
+    component: SeoTekniskComponent,
+  },
+  {
+    path: 'tjenester/bedrift/seo/innholdsproduksjon',
+    component: SeoInnholdsproduksjonComponent,
+  },
+  {
+    path: 'tjenester/bedrift/seo/off-page-seo',
+    component: SeoOffPageComponent,
+  },
   { path: 'tjenester/bedrift/utvikling', component: UtviklingComponent },
+  {
+    path: 'tjenester/bedrift/utvikling/hjemmeside-bedrift',
+    component: UtviklingHjemmesideComponent,
+  },
+  {
+    path: 'tjenester/bedrift/utvikling/datasystem',
+    component: UtviklingSystemComponent,
+  },
 ];
 
 @NgModule({
@@ -27,6 +67,14 @@ const routes: Routes = [
     TjenesterServicesComponent,
     TjenesterInfoSectionComponent,
     CtaCardComponent,
+    UtviklingHjemmesideComponent,
+    UtviklingSystemComponent,
+    SeoTekniskComponent,
+    SeoInnholdsproduksjonComponent,
+    SeoOffPageComponent,
+    DesignGrafiskComponent,
+    DesignLogoComponent,
+    DesignWebComponent,
   ],
   imports: [CommonModule, RouterModule.forRoot(routes)],
 })
