bc1c914bd34a2eda0c6445a6f6d1b15ef6f5750e
Stian have <stianhave@hotmail.com>
2022-03-20 00:22:58 +0100

    added price utils

---
+++ ModernaMediaAngular/src/utils/PriceUtils.ts
@@ -0,0 +1,36 @@
+export const PriceUtils = {
+  Utvikling: {
+    Nettside: {
+      1: { name: 'enkel hjemmeside', price: '10.000' },
+      2: { name: 'medium hjemmeside', price: '22.000' },
+      3: { name: 'standard hjemmeside', price: '55.000' },
+      4: { name: 'kompleks hjemmeside', price: '125.000' },
+    },
+    Programvare: {
+      1: { name: 'standard hjemmeside', price: '50.000' },
+      2: { name: 'standard hjemmeside', price: '125.000' },
+    },
+  },
+  Design: {
+    Logo: {
+      1: { name: 'standard hjemmeside', price: '7.500' },
+    },
+    Web: {
+      1: { name: 'standard hjemmeside', price: '25.000' },
+    },
+    Grafisk: {
+      1: { name: 'standard hjemmeside', price: '25.000' },
+    },
+  },
+  SEO: {
+    Teknisk: {
+      1: { name: 'standard hjemmeside', price: '15.000' },
+    },
+    OffPage: {
+      1: { name: 'standard hjemmeside', price: '15.000' },
+    },
+    Innholdsproduksjon: {
+      1: { name: 'standard hjemmeside', price: '3.000' },
+    },
+  },
+};
