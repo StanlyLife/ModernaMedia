b695eea65562128c12efa9d558f00b238c78a94c
Stian have <stianhave@hotmail.com>
2022-03-20 19:41:42 +0100

    Capitalized letter

--- ModernaMediaAngular/src/utils/BlogUtils.ts
+++ ModernaMediaAngular/src/utils/BlogUtils.ts
@@ -5,7 +5,7 @@ export const Blogs = [
     Author: 'Stian Håve',
     Organization: 'Moderna Media',
     Description:
-      'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
+      'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? Hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
     url: '/blogg/utviklerlonn',
     img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
     alt: 'Graf som viser inntekt og lønn for utviklere',
