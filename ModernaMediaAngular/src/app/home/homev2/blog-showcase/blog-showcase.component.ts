b4c4cd2cf58fbd954d39feefb6de50819d5a4bf0
Stian have <stianhave@hotmail.com>
2022-03-20 12:12:04 +0100

    Extracted blogs to blogutils

--- ModernaMediaAngular/src/app/home/homev2/blog-showcase/blog-showcase.component.ts
+++ ModernaMediaAngular/src/app/home/homev2/blog-showcase/blog-showcase.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Blogs } from 'src/utils/BlogUtils';
 @Component({
   selector: 'app-blog-showcase',
   templateUrl: './blog-showcase.component.html',
@@ -14,74 +15,7 @@ export class BlogShowcaseComponent implements OnInit {
   sanitizeImageUrl(imageUrl: string): SafeUrl {
     return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
   }
-  blogs = [
-    {
-      Published: '16 DES 2021',
-      Title: 'Utvikler lønn',
-      Author: 'Stian Håve',
-      Organization: 'Moderna Media',
-      Description:
-        'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
-      url: '/',
-      img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
-      alt: '',
-    },
-    {
-      Published: '16 DES 2021',
-      Title: 'Utvikler lønn',
-      Author: 'Stian Håve',
-      Organization: 'Moderna Media',
-      Description:
-        'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
-      url: '/',
-      img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
-      alt: '',
-    },
-    {
-      Published: '16 DES 2021',
-      Title: 'Utvikler lønn',
-      Author: 'Stian Håve',
-      Organization: 'Moderna Media',
-      Description:
-        'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
-      url: '/',
-      img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
-      alt: '',
-    },
-    {
-      Published: '16 DES 2021',
-      Title: 'Utvikler lønn',
-      Author: 'Stian Håve',
-      Organization: 'Moderna Media',
-      Description:
-        'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
-      url: '/',
-      img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
-      alt: '',
-    },
-    {
-      Published: '16 DES 2021',
-      Title: 'Utvikler lønn',
-      Author: 'Stian Håve',
-      Organization: 'Moderna Media',
-      Description:
-        'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
-      url: '/',
-      img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
-      alt: '',
-    },
-    {
-      Published: '16 DES 2021',
-      Title: 'Utvikler lønn',
-      Author: 'Stian Håve',
-      Organization: 'Moderna Media',
-      Description:
-        'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
-      url: '/',
-      img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
-      alt: '',
-    },
-  ];
+  blogs = Blogs;
   imageCdn = environment.img;
   ngOnInit(): void {}
 }
---
+++ ModernaMediaAngular/src/utils/BlogUtils.ts
@@ -0,0 +1,13 @@
+export const Blogs = [
+  {
+    Published: '16 DES 2021',
+    Title: 'Utvikler lønn',
+    Author: 'Stian Håve',
+    Organization: 'Moderna Media',
+    Description:
+      'Hva tjente utviklerne i 2021 og har det vært en generell økning fra 2020? hvorfor har de med bonus også mest lønn? Gjennom artikkelen vil vi se på data samlet fra 1529 utviklere i Norge.',
+    url: '/blogg/utviklerlonn',
+    img: '../../../../assets/Images/Home/blog/400/Utviklere-som-lager-bedrift-system-og-nettsider.webp',
+    alt: '',
+  },
+];
