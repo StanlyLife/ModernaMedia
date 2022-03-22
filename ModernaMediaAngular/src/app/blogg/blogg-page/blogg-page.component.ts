afc757ce905562bd8e8c3d7ea5deed722ab00500
Stian have <stianhave@hotmail.com>
2022-03-22 20:19:18 +0100

    created blogg page component

---
+++ ModernaMediaAngular/src/app/blogg/blogg-page/blogg-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogg-page',
+  templateUrl: './blogg-page.component.html',
+  styleUrls: ['./blogg-page.component.scss']
+})
+export class BloggPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
