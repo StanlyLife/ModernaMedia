2a5ce2dbdaaa4b9a08f601147d1a7ed5c32d9e6b
Stian have <stianhave@hotmail.com>
2022-03-22 22:56:32 +0100

    updated canoical tags

--- ModernaMediaAngular/src/app/services/seo.service.ts
+++ ModernaMediaAngular/src/app/services/seo.service.ts
@@ -1,18 +1,15 @@
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SeoService {
   constructor(@Inject(DOCUMENT) private doc) {}
-  
-  createLinkForCanonicalURL() {
-     let link: HTMLLinkElement = this.doc.createElement('link');
-     if((link + "").startsWith("https://") ){
 
-       link.setAttribute('rel', 'canonical');
-       this.doc.head.appendChild(link);
-       link.setAttribute('href', this.doc.URL);
-      }
-   }
+  createLinkForCanonicalURL() {
+    let link: HTMLLinkElement = this.doc.createElement('link');
+    link.setAttribute('rel', 'canonical');
+    this.doc.head.appendChild(link);
+    link.setAttribute('href', this.doc.URL);
+  }
 }
