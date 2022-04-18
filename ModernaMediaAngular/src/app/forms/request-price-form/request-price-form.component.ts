874ecd685ab434b5e3bcc6fd26d98755cadd09ab
Stian have <stianhave@hotmail.com>
2022-04-18 18:04:55 +0200

    added request price component

--- ModernaMediaAngular/src/app/forms/request-price-form/request-price-form.component.ts
+++ ModernaMediaAngular/src/app/forms/request-price-form/request-price-form.component.ts
@@ -1,13 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { SeoService } from 'src/app/services/seo.service';
 import { SeoUtils } from 'src/utils/SeoUtils';
-
+import { ContactService } from './../../services/contact.service';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { DomSanitizer, SafeUrl, Meta, Title } from '@angular/platform-browser';
+import { ViewportScroller } from '@angular/common';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-request-price-form',
   templateUrl: './request-price-form.component.html',
-  styleUrls: ['./request-price-form.component.scss'],
+  styleUrls: [
+    './request-price-form.component.scss',
+    '../contact-form/contact-form.component.scss',
+    '../contact-form/contact-form.desktop.component.scss',
+  ],
 })
 export class RequestPriceFormComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(
+    private sanitizer: DomSanitizer,
+    private scroller: ViewportScroller,
+    private fb: FormBuilder,
+    private cs: ContactService,
+    private meta: Meta,
+    private title: Title,
+    private seo: SeoService
+  ) {
+    title.setTitle(SeoUtils.FromsPrice.title);
+  }
+  ngOnInit(): void {
+    this.seo.createLinkForCanonicalURL();
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: SeoUtils.FromsPrice.description,
+      },
+      { name: 'robots', content: 'index, follow' },
+    ]);
+    this.meta.addTag({
+      name: 'angular.ModernaMedia',
+      content: 'ModernaMedia',
+    });
+  }
+  @Input() data: any = {
+    background: {
+      alt: '',
+      src: '../../../../assets/Images/forms/contact/trollstigen i molde eller kristansund.jpg',
+    },
+    title: 'Få et uforpliktet pristilbud',
+    subtitle: '100% fornøyd garanti på nettsider, programvare og design!',
+  };
+  sanitizeImageUrl(imageUrl: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
+  contactForm = this.fb.group({
+    name: [''],
+    email: ['', Validators.required],
+    phone: [''],
+    business: [''],
+    service: ['nettside'],
+    body: ['', Validators.required],
+  });
+  result = false;
+  sent = false;
+  onSubmit(): void {
+    if (this.result || this.sent) {
+      return;
+    }
+    this.sent = true;
+    var request = this.cs.SendContactRequest(this.contactForm.value);
+    this.cs.SendContactRequestResult.subscribe((arg) => {
+      this.result = arg;
+      if (this.result) {
+        this.contactForm.reset();
+      }
+    });
+  }
+  formError =
+    !this.contactForm.valid &&
+    this.contactForm.touched &&
+    ((!this.contactForm.controls['email'].valid &&
+      this.contactForm.controls['email'].touched) ||
+      // (!this.contactForm.controls['service'].valid &&
+      //   this.contactForm.controls['service'].touched) ||
+      (!this.contactForm.controls['body'].valid &&
+        this.contactForm.controls['body'].touched));
+  ChangingValue(e) {
+    console.log(e);
+  }
 }
